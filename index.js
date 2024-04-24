ba321e1238945a7385db056030da79894627530b
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-24 11:33:12 +0600

    Add database

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -6,6 +7,65 @@ const port = process.env.PORT || 5000;
 // middlewere
 app.use(cors());
 app.use(express.json());
+
+const uri =
+  "mongodb+srv://Book_Store:X0rPMGu5I77GN4uy@cluster0.dbn21dt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const bookCollection = client.db("bookDB").collection("book");
+
+    app.get("/book", async (req, res) => {
+      const cursor = bookCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/book", async (req, res) => {
+      const newBook = req.body;
+      const result = await bookCollection.insertOne(newBook);
+      res.send(result);
+    });
+
+    app.put("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateBook = req.body;
+      const book = {
+        $set: {
+          name: updateBook.name,
+          photo: updateBook.photo,
+          descrip: updateBook.descrip,
+        },
+      };
+      const result = await bookCollection.updateOne(query, book, options);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server Is runing with Full speed");
