b54a1155389f6673495645f5902fc4a3f73747df
Muhammad Sefat <muhammadsefat55@gmail.com>
2024-04-24 19:32:58 +0600

    Final Commit

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
